f31739120e4357ec0914cb9846c5c1f379e1ea37
vedamshpawar <vedamshpawar@gmail.com>
2025-12-27 21:21:42 +0530

    added imgs and registered users

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'posts'
+    'posts',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -120,6 +121,11 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
+
+
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
